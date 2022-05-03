02d312adf45686526a9918b4aa1cc37320ec316f
Avivo <avielber26@gmail.com>
2022-05-03 11:30:01 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+          <h1>Gérez votre abonnement et votre compte</h1>
+          <h2>Une gestion facile et innovante</h2>
+          <div class="d-flex justify-content-center justify-content-lg-start">
+            <a href="{{ route('logout') }}" class="btn-get-started scrollto">Se déconnecter</a>
+          </div>
         </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+          <img src="{{ asset('Arsha/img/hero-img.png') }}" class="img-fluid animated" alt="">
+        </div>
+      </div>
     </div>
-</div>
+
+  </section><!-- End Hero -->
 @endsection
